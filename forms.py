19ff168589750f768bac4cfa3772643a9fa1e8f4
Virginia Njenga <njenganv@gmail.com>
2020-07-02 14:23:36 -0700

    webapp now works

--- forms.py
+++ forms.py
@@ -4,6 +4,12 @@ from wtforms.validators import DataRequired, Length
 
 class SearchForm(FlaskForm):
     seed = StringField('Seed')
-    dropdown_choices = 
-    book = SelectField('Book', choices=[(0,"The Picture of Dorian Gray"),(1,"WIP"),(2,"Lol")])
+    #'''
+    dropdown_choices = [('0','The Picture of Dorian Gray'),
+    						('1','Pride and Prejudice'),
+    						('2','A Take of Two Cities'),
+    						('3','The Brothers Karamazov'),
+    						('4','The Souls of Black Folk')]
+    #'''
+    book = SelectField('Book', choices=dropdown_choices)
     submit = SubmitField('Submit')
--- webapp.py
+++ webapp.py
@@ -3,26 +3,28 @@ from forms import SearchForm
 import requests
 import json
 import unicodedata
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '3141592653589793238462643383279502884197169399'
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
 	form = SearchForm()
 	if form.validate_on_submit():
-		print('YAYYY')
 		redirect_url = url_for('text', book=form.book.data, seed=form.seed.data)
-		return redirect(redirect_ur)
-	print(form.errors)
+		return redirect(redirect_url)
+	#print(form.errors)
 	return render_template('search.html', form=form)
 	
 
 @app.route('/text')
-def text(book,seed):
-	#r = request.args
-	#api_url = 'http://localhost:8080/generate-text?book='+r['book']+'&seed='+r['seed']
-	api_url = 'http://localhost:8080/generate-text?book='+book+'&seed='+seed
+def text():
+	r = request.args
+	api_url = 'http://localhost:8080/generate-text?book='+r['book']+'&seed='+r['seed']
+	#api_url = 'http://localhost:8080/generate-text?book='+book+'&seed='+seed
+	print('Got url:',api_url)
 	info = requests.get(api_url)
 	info = unicodedata.normalize('NFKD', info.text).encode('ascii','ignore')
 	info = json.loads(info)
